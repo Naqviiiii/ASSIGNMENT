b1ea4fa32b9fd3797d99374132365cd3ffb808e5
Naqviiiii <syedmuhammadakber6@gmail.com>
2026-01-11 19:57:10 +0500

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Power {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter your Voltage reading :");
+        Double Volts=input.nextDouble();
+        System.out.print("Enter your Ampere reading :");
+        double Ampere=input.nextDouble();
+        double Watt=Volts*Ampere;
+        System.out.print("Your total power calculated is :" + Watt);
+    }
+}
