ba2817c7b4f0c65795ade7b9471229a0bf966588
Naqviiiii <syedmuhammadakber6@gmail.com>
2026-01-11 19:49:07 +0500

    Add files via upload

---
+++ OriginalPrice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class OriginalPrice {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter your discounted Price :");
+        double DSP = input.nextDouble();
+        System.out.print("Enter your discount percentage :");
+        double DP=input.nextDouble();
+        double OP=DSP/(1-(DP/100));
+        System.out.print("The original price is :" + OP);
+    }
+    
+}
